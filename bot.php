66a1a9959de1ce2bf2f2d7dbc14449d9d534590e
yokeap <siwakorn.ictrl@gmail.com>
2017-12-15 20:46:59 +0700

    overall changes with nuuneoi.

--- bot.php
+++ bot.php
@@ -1,44 +1,47 @@
 <?php
-require('vendor/autoload.php');
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
+$access_token = 'SCV7PNRDb7/XCuNp5C7L3n25Sv4GsKuM9zRxy5+7cBCOl7QzhQloM1WUysJ/dytJOmAuNL9K/XAdrGrmieVADiWY/uIA4lZdgWF5LQUUosltryHc2JyEcz/dgujCXoF0joDF0Z84GLmydZituZPQRAdB04t89/1O/w1cDnyilFU=';
 
-require('phpMQTT.php');
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$mqtt = new phpMQTT('www.m14.cloudmqtt.com', 19348, 'phpMQTT Pub Example'); /
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-$token = 'SCV7PNRDb7/XCuNp5C7L3n25Sv4GsKuM9zRxy5+7cBCOl7QzhQloM1WUysJ/dytJOmAuNL9K/XAdrGrmieVADiWY/uIA4lZdgWF5LQUUosltryHc2JyEcz/dgujCXoF0joDF0Z84GLmydZituZPQRAdB04t89/1O/w1cDnyilFU='; 
-$httpClient = new CurlHTTPClient($token);
-$bot = new LINEBot($httpClient, [‘channelSecret’ => $token]);
-// webhook
-$jsonStr = file_get_contents(‘php://input’);
-$jsonObj = json_decode($jsonStr);
-print_r($jsonStr);
-foreach ($jsonObj->events as $event) {
-if(‘message’ == $event->type){
-// debug
-//file_put_contents('message.json', json_encode($event));
-$text = $event->message->text;
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-if (preg_match('test', $text)) {
-$text = 'Yokeap';
-}
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-if (preg_match('/pump/', $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
-if ($mqtt->connect()) {
-$mqtt->publish('/ESP/REMOTE','TV'); // ตัวอย่างคำสั่งเปิดทีวีที่จะส่งไปยัง mqtt server
-$mqtt->close();
+			echo $result . "\r\n";
+		}
+	}
 }
-$text = 'เปิดทีวีให้แล้วคร้าบบบบ';
-}
-if (preg_match('/ปิดทีวี/', $text) and !preg_match('/เปิดทีวี/', $text)) {
-if ($mqtt->connect()) {
-$mqtt->publish('/ESP/REMOTE','TV');
-$mqtt->close();
-}
-$text = 'จ่าปิดทีวีให้แล้วนะครับ!!';
-}
-$response = $bot->replyText($event->replyToken, $text); // ส่งคำ reply กลับไปยัง line application
-
-}
-}
+echo "OK";
