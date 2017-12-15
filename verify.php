2cbdf2ce0e6633c592676883dacd1695e21c8953
yokeap <siwakorn.ictrl@gmail.com>
2017-12-16 00:20:23 +0700

    debug3

--- verify.php
+++ verify.php
@@ -1,11 +1,16 @@
 <?php
-echo "MQTT Testing\n";
+//echo "MQTT Testing\n";
 
-require("phpMQTT.php");
+//require("phpMQTT.php");
+
+include ("phpMQTT.php");
+
+echo "MQTT Testing2\n";
+
 $server = "m14.cloudmqtt.com";     // change if necessary
 $port = 19348;                     // change if necessary
 $username = "vidaaruu";                   // set your username
-$password = "Ro2sY3zEhY9W";                   // set your password
+$password = "TEST";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 $mqtt = new phpMQTT($server, $port, $client_id);
 echo "connecting to MQTT Server\n";
