8b6c34e268e73ad246d6b07f8af24247a4208a75
yokeap <siwakorn.ictrl@gmail.com>
2017-12-16 01:47:14 +0700

    changed

--- verify.php
+++ verify.php
@@ -1,18 +1,16 @@
 <?php
 //echo "MQTT Testing\n";
 
-require("phpMQTT.php");
+require('vendor/autoload.php');
 
 //include ("phpMQTT.php");
 
-echo "MQTT Testing2\n";
-
 $server = "m14.cloudmqtt.com";     // change if necessary
-$port = 39348;                     // change if necessary
-$username = "TEST";                   // set your username
-$password = "12345";                   // set your password
-$client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
-$mqtt = new phpMQTT($server, $port, $client_id);
+$port = 19348;                     // change if necessary
+$username = "vidaaruu";                   // set your username
+$password = "Ro2sY3zEhY9W";                   // set your password
+$client_id = "phpMQTT-publisher"; 
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
 echo "connecting to MQTT Server\n";
 if ($mqtt->connect(true, NULL, $username, $password)) {
 	$mqtt->publish("/ESP/REMOTE", "LED", 0);
