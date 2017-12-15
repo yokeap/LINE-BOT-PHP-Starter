7a0272f646976fba330a589dec5cbc648b26b29f
yokeap <siwakorn.ictrl@gmail.com>
2017-12-16 02:06:28 +0700

    update bot.php

--- bot.php
+++ bot.php
@@ -4,17 +4,18 @@
 
 $access_token = 'SCV7PNRDb7/XCuNp5C7L3n25Sv4GsKuM9zRxy5+7cBCOl7QzhQloM1WUysJ/dytJOmAuNL9K/XAdrGrmieVADiWY/uIA4lZdgWF5LQUUosltryHc2JyEcz/dgujCXoF0joDF0Z84GLmydZituZPQRAdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-
 $server = "m14.cloudmqtt.com";     // change if necessary
 $port = 19348;                     // change if necessary
 $username = "vidaaruu";                   // set your username
 $password = "Ro2sY3zEhY9W";                   // set your password
 $client_id = "phpMQTT-publisher"; 
 $mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+
 
 echo "connecting to MQTT Server\n";
 
