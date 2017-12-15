f59bc96c2c422f40a3306a61e03605d937fbc040
yokeap <siwakorn.ictrl@gmail.com>
2017-12-15 23:31:03 +0700

    test

--- verify.php
+++ verify.php
@@ -1,4 +1,5 @@
 <?php
+echo "MQTT Testing\n";
 
 require("phpMQTT.php");
 $server = "m14.cloudmqtt.com";     // change if necessary
@@ -7,7 +8,7 @@ $username = "vidaaruu";                   // set your username
 $password = "Ro2sY3zEhY9W";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 $mqtt = new phpMQTT($server, $port, $client_id);
-echo "connecting to MQTT Server\n"
+echo "connecting to MQTT Server\n";
 if ($mqtt->connect(true, NULL, $username, $password)) {
 	$mqtt->publish("/ESP/REMOTE", "LED", 0);
 	$mqtt->close();
