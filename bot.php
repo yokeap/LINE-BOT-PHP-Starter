e8dca18cd00516f0ca528e2ffef9169fcb1570f1
yokeap <siwakorn.ictrl@gmail.com>
2017-12-27 15:11:00 +0700

    remove function

--- bot.php
+++ bot.php
@@ -99,35 +99,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-
-function LineReply($text){
-	// Get replyToken
-	$replyToken = $event['replyToken'];
-
-	// Build message to reply back
-	$messages = [
-		'type' => 'text',
-		'text' => $text
-	];
-
-	// Make a POST Request to Messaging API to reply to sender
-	$url = 'https://api.line.me/v2/bot/message/reply';
-	$data = [
-		'replyToken' => $replyToken,
-		'messages' => [$messages],
-	];
-	$post = json_encode($data);
-	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$result = curl_exec($ch);
-	curl_close($ch);
-
-	echo $result . "\r\n";
-}
 echo "OK";
